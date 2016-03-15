94a40abff102647339bb8ca11a372e67f7e8a296
David <Dgbaker93@gmail.com>
2016-03-15 17:03:28 -0400

    Setup the SettingsActivity

---
+++ app/src/main/java/com/example/home/android_project_3/SettingsActivity.java
@@ -0,0 +1,40 @@
+package com.example.home.android_project_3;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Build;
+import android.preference.PreferenceFragment;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        FragmentManager mFragmentManager = getFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager
+                .beginTransaction();
+        PrefsFragment mPrefsFragment = new PrefsFragment();
+        mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
+        mFragmentTransaction.commit();
+
+
+    }
+
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @TargetApi(Build.VERSION_CODES.M)
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
+    }
+}
