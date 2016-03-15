4a1f7923a5f891cc1684eb8006738084bfb2be17
David <Dgbaker93@gmail.com>
2016-03-15 16:38:12 -0400

    Initial commit

---
+++ app/src/androidTest/java/com/example/home/android_project_3/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.home.android_project_3;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
