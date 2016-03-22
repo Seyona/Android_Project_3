ce94f8cc386a493bfed1f8e735d11293cccca3ad
David <Dgbaker93@gmail.com>
2016-03-22 15:24:44 -0400

    Added Toasts for when there is no connection to the Internet, and when there is no Network connection

--- app/src/main/java/com/example/home/android_project_3/MainActivity.java
+++ app/src/main/java/com/example/home/android_project_3/MainActivity.java
@@ -11,12 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.prefs.PreferenceChangeListener;
 
@@ -25,11 +27,13 @@ public class MainActivity extends AppCompatActivity {
     private static final boolean NO_NETWORK_ACCESS = false;
     private SharedPreferences prefs = null;
     private SharedPreferences.OnSharedPreferenceChangeListener listener;
+    private ArrayList<Pet> pets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
             listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
                 public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
@@ -40,11 +44,10 @@ public class MainActivity extends AppCompatActivity {
             };
             prefs.registerOnSharedPreferenceChangeListener(listener);
 
-        if (isOnline()) {
+        if (!isOnline()) {
             // check for website connectivity
-
-        } else {
             Log.e("Connection","No_Connection");
+
         }
 
 
@@ -68,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
                 int exitValue = ipProcess.waitFor();
+                if (exitValue != 0) {
+                    Toast.makeText(this, "You are connected to a network, but there is not access to the internet", Toast.LENGTH_SHORT);
+                }
                 return (exitValue == 0);
 
             } catch (IOException e) {
@@ -77,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        Toast.makeText(this,"You are not connected to a network", Toast.LENGTH_SHORT);
         return NO_NETWORK_ACCESS;
     }
 
